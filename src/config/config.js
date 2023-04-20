5fed5c7789fffcfe91201968b189a03b48122aed
alomerovickarim47 <karimalomerovic@yahoo.com>
2023-04-20 23:32:33 +0200

    env var tweak

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
-    BACKEND_URL: process.env.NODE_ENV === "production"
-    ?"https://akdevbackend.onrender.com"
-    :"http://localhost:4000"
+    BACKEND_URL: process.env.NODE_ENV === "development"
+    ?"http://localhost:4000"
+    :"https://akdevbackend.onrender.com"
 }
 
 export default config
